2e6ba310f39f560023307252b6df6b12a07c7307
Brian Malloy <malloy@clemson.edu>
2016-02-01 15:52:10 -0500

    new changes

--- patterns/singleton/after/main.cpp
+++ patterns/singleton/after/main.cpp
@@ -3,19 +3,33 @@
 
 class Random {
 public:
-  Random() { 
-    int seed = time(0);
-    srand(seed); 
-  }
+  static Random* getInstance();
   int operator()(int a, int b) const { 
     return (rand() % b) + a; 
   }
 private:
+  static Random* instance;
+  int x;
+  Random();
   Random(const Random&);
   Random& operator=(const Random&);
 };
 
+Random* Random::getInstance() {
+  if ( !instance ) instance = new Random;
+  return instance;
+}
+
+Random::Random() { 
+  int seed = time(0);
+  srand(seed); 
+}
+
+Random* Random::instance = NULL;
+Random::x = 99;
+
 int main() {
-  Random random;
-  std::cout << random(1,100) << std::endl;
+  Random* random = Random::getInstance();
+  std::cout << (*random)(1,100) << std::endl;
+  delete random;
 }
