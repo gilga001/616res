b70560a5706d31c5dec8b74349537d40acea5c8a
Brian Malloy <malloy@clemson.edu>
2016-02-01 20:37:56 -0500

    new changes

--- examples/cpp/play/main.cpp
+++ examples/cpp/play/main.cpp
@@ -9,6 +9,7 @@ int main() {
   std::cout << "x is: " << static_cast<int>(x) << std::endl;
   std::cout << "i is: " << i << std::endl;
   std::cout << "i+1 is: " << i+1 << std::endl;
+  std::cout << "i+1 is: " << static_cast<unsigned int>(i)+1 << std::endl;
   std::cout << "j+1 is: " << j+1 << std::endl;
   std::cout << "2j+1 is: " << 2*(j+1) << std::endl;
   std::cout << "biggest int: " << std::numeric_limits<int>::max() 
